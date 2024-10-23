751c3db9816d5507b408b919fff067f0a9a22535
Luana <luanap.teixeira@hotmail.com>
2024-10-23 17:00:54 -0300

    css

--- pages/homepage/index.php
+++ pages/homepage/index.php
@@ -4,13 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial</title>
+    <link rel="stylesheet" type="text/css" href="../../assets/mobile.css" media="screen and (max-width: 600px)">
+    <link rel="stylesheet" type="text/css" href="../../assets/desktop.css" media="screen and (min-width: 601px)">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
+    <?php include 'header.php'; ?>
+
     <div class="container">
         <h1>CritMeet</h1>
         <button type="button">Notificação de Acontecimentos</button><br>
         <button type="button">Notificação de Mensagens</button><br>
         <button type="button">Sessões Agendadas</button>
     </div>
+
+    <?php include 'footer.php'; ?>
 </body>
 </html>
--- pages/login/index.php
+++ pages/login/index.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../../assets/mobile.css" media="screen and (max-width: 600px)">
+    <link rel="stylesheet" type="text/css" href="../../assets/desktop.css" media="screen and (min-width: 601px)">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Agdasima:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
+    <?php include 'header.php'; ?>
+
     <div class="container">
         <h1>CritMeet</h1><br>
         <input type="text" placeholder="Login" /><br>
@@ -16,5 +23,7 @@
         <button type="button">Entrar</button><br>
         <button type="button">Google</button>
     </div>
+
+    <?php include 'footer.php'; ?>
 </body>
 </html>
