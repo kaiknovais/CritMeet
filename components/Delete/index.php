79f458c20fd1a81b2ca5e43facb766d6baf36146
Luana <>
2024-11-27 17:19:04 -0300

    correção de delete

--- components/Delete/index.php
+++ components/Delete/index.php
@@ -7,18 +7,26 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 
-if (isset($_POST['delete_user'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_user'])) {
     $user_id = $_SESSION['user_id'];
 
-    $stmt = $mysqli->prepare("DELETE FROM users WHERE id = ?");
-    $stmt->bind_param("i", $user_id);
+    if ($stmt = $mysqli->prepare("DELETE FROM users WHERE id = ?")) {
+        $stmt->bind_param("i", $user_id);
 
-    if ($stmt->execute()) {
-        session_destroy();
-        header("Location: ../../login/index.php");
-        exit();
+        if ($stmt->execute()) {
+            session_destroy();
+            header("Location: ../../pages/home/index.php");
+            exit();
+        } else {
+            echo "Erro ao excluir a conta: " . $stmt->error;
+        }
+
+        $stmt->close();
     } else {
-        echo "Erro ao excluir a conta: " . $stmt->error;
+        echo "Erro na preparação do SQL: " . $mysqli->error;
     }
 }
+
+
+$mysqli->close();
 ?>
